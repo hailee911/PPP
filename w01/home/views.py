9974e38fd983d5a711e973aaf1309ab313397ea6
hyundingi <qo0723@naver.com>
2024-12-23 21:26:18 +0900

    1223 현지 집가자

--- w01/home/views.py
+++ w01/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from loginpage.models import Member
 from diary.models import Content
+from customer.models import NoticeBoard
 from django.http import JsonResponse,HttpResponse
 from django.db.models import Q
 
@@ -10,12 +11,13 @@ def landing(request):
   return render(request,'landing.html')
 
 def main(request):
-  return render(request, 'main.html')
+  qs_post = NoticeBoard.objects.filter(category=2).order_by('-bno')
+  context = {'post_lists':qs_post}
+  return render(request, 'main.html', context)
 
 def logout(request):
-  response = redirect('/')
-  response.delete_cookie('sessionid')  # 세션 쿠키 삭제
-  return response
+  request.session.clear()
+  return redirect('/')
 
 # 검색창
 def search(request):
